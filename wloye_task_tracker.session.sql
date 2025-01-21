8df8202b39e7a45fc58772fbc3185e922a09bf5e
ma09ry90 <118520635+ma09ry90@users.noreply.github.com>
2025-01-22 01:02:15 +0300

    create task table

--- wloye_task_tracker.session.sql
+++ wloye_task_tracker.session.sql
@@ -1,1 +1,9 @@
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(50),
+    due_date DATE,
+    attachment VARCHAR(255)
+);
 UPDATE tasks SET attachment = REPLACE(attachment, 'uploads/uploads/', 'uploads/');
